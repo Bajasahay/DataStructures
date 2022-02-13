87dd919f28870438fd3734032fec793f1c2a5d05
Abhilekh Sahay <asahay@Abhilekhs-MacBook-Air.local>
2022-02-13 23:23:35 +0530

    Weekly Contest 280

---
+++ src/LeetCode/WeeklyContest280/CountOperations.java
@@ -0,0 +1,18 @@
+package LeetCode.WeeklyContest280;
+
+public class CountOperations {
+
+    public static Integer check(Integer num1, Integer num2) {
+        Integer count = 0;
+        while(num1!=0 && num2!=0){
+            if(num1>=num2) {
+                num1 = num1-num2;
+                count = count+1;
+            } else {
+                num2 = num2-num1;
+                count = count+1;
+            }
+        }
+        return count;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
 import Recursion.RecursionMain;
 import Tree.TreeMain;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String args[]) {
         System.out.println("Main Method");
 //        TreeMain treeMain = new TreeMain();
 //        treeMain.executeCode();
-        RecursionMain recursionMain = new RecursionMain();
-        recursionMain.executeCode();
+//        RecursionMain recursionMain = new RecursionMain();
+//        recursionMain.executeCode();
     }
 }
